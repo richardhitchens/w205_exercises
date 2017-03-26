07f3aabdcc04cb909c8a89fe805ffe59d18849bb
Richard Hitchens <Richard@Richards-MacBook-Pro.local>
2017-03-27 10:04:55 +1100

    storm files

--- exercise_2/extweetwordcount/src/bolts/wordcount.py
+++ exercise_2/extweetwordcount/src/bolts/wordcount.py
@@ -4,6 +4,7 @@ from collections import Counter
 from streamparse.bolt import Bolt
 
 
+
 class WordCounter(Bolt):
 
     def initialize(self, conf, ctx):
@@ -11,6 +12,17 @@ class WordCounter(Bolt):
 
     def process(self, tup):
         word = tup.values[0]
+
+        # Write codes to increment the word count in Postgres
+        # Use psycopg to interact with Postgres
+        # Database name: Tcount 
+        # Table name: Tweetwordcount 
+        # you need to create both the database and the table in advance.
+        
+
+        # Increment the local count
         self.counts[word] += 1
         self.emit([word, self.counts[word]])
+
+        # Log the count - just to see the topology running
         self.log('%s: %d' % (word, self.counts[word]))
